5bd8484c9a07210770b2a9c4221f85128ad3119b
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-18 14:41:21 +1100

    add softmax utils

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,21 @@ get_file <- function(model) {
     stop("Model type not supported", call. = FALSE)
   }
 }
+
+#' Numerically stable log_sum_exp function
+logsumexp <- function (x) {
+    y <- max(x)
+    y + log(sum(exp(x - y)))
+  }
+
+#' Softmax function
+softmax <- function (x) {
+  exp(x - logsumexp(x))
+}
+
+#' Check that a passed object is actually of type fit
+validate_fit <- function(fit) {
+  if (all(class(fit) != "fit")) {
+    stop("Cannot plot a non-fit object", call. = FALSE)
+  }
+}
