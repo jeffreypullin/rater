ba215ad16f2d1e46b10b1282c934f0e3818b4807
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-18 12:24:16 +1100

    add fit_class

---
+++ R/fit_class.R
@@ -0,0 +1,16 @@
+print.fit <- function(fit) {
+
+  cat("Model:\n\n")
+  print(fit$model)
+  cat("\n\n")
+
+  max.print_default <- options("max.print")
+  options(max.print = 80)
+
+  cat("Samples:\n\n")
+  print(fit$draws)
+  cat("\n")
+
+  options(max.print = max.print_defaul)
+
+}
