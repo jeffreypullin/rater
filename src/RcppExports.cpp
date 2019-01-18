ab03ced7e82d13176fbcede59614deae11ce488b
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-18 14:42:13 +1100

    add source files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,9 +7,11 @@
 using namespace Rcpp;
 
 
+RcppExport SEXP _rcpp_module_boot_stan_fit4dawid_skene_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4lm_mod();
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_rcpp_module_boot_stan_fit4dawid_skene_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4dawid_skene_mod, 0},
     {"_rcpp_module_boot_stan_fit4lm_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4lm_mod, 0},
     {NULL, NULL, 0}
 };
