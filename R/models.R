9c2b77f84d4458e045186c967674fcbdeb0bb351
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-20 16:02:02 +1100

    clean up models

--- R/models.R
+++ R/models.R
@@ -12,8 +12,8 @@ NULL
 #' @rdname models
 #' @export
 #'
-#' @param alpha
-#' @param beta
+#' @param alpha prior parameter for pi
+#' @param beta prior parameter for theta
 #'
 dawid_skene <- function(alpha = NULL,
                         beta = NULL) {
@@ -24,27 +24,4 @@ dawid_skene <- function(alpha = NULL,
   class(obj) <- c("dawid_skene", "model")
 
   obj
-}
-
-#' print a fit model object
-#' @export
-print.model <- function(x, print_code = FALSE) {
-  cat(get_name(x), "\n\n")
-  params <- x$parameters
-
-  cat("Prior parameters:\n\n")
-
-  for (i in 1:length(params)) {
-    cat(paste0(names(params)[[i]], ":"))
-
-    if (!is.null(params[[i]])) {
-
-      cat("\n \n")
-      print(params[[i]])
-      cat("\n")
-
-    } else {
-      cat(" default\n")
-    }
-  }
 }
