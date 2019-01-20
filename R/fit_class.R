cabc5dca3607a5c2ae83d2e8b6d32cf8e386bbb0
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-20 13:34:12 +1100

    add plot.fit

--- R/fit_class.R
+++ R/fit_class.R
@@ -16,3 +16,31 @@ print.fit <- function(fit) {
   options(max.print = max.print_default)
 
 }
+
+
+#' Plot a fit object
+#' @param fit a fit object
+#' @param type what sort of plot should be plotted
+#'
+#' @export
+plot.fit <- function(fit, type = c("theta", "raters",
+                                   "pi", "prevalance",
+                                   "z", "latent_class")) {
+  type <- match.arg(type)
+
+  if (type %in% c("theta", "raters")) {
+
+    plot <- plot_raters(fit)
+
+  } else if (type %in% c("pi", "prevalance")) {
+
+    plot <- plot_prevalance(fit)
+
+  } else if (type %in% c("z", "latent_class")) {
+
+    plot_latent_class(fit)
+
+  }
+
+  plot
+}
