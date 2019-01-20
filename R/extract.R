ed0b191c65ef9746b9eff3d30d5395c7cb9f8a91
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-01-20 16:02:37 +1100

    add extract_prevalance

--- R/extract.R
+++ R/extract.R
@@ -4,7 +4,6 @@
 #' @return Probalistic latent class measurements
 #'
 #' @export
-
 extract_latent_class <- function(fit) {
 
   validate_fit(fit)
@@ -14,6 +13,8 @@ extract_latent_class <- function(fit) {
   I <- dim(log_p_z_samps)[[2]]
   K <- dim(log_p_z_samps)[[3]]
 
+
+  # TODO probably should mean and then softmax...
   log_p_z <- matrix(0, nrow = I, ncol = K)
   for(i in 1:I){
     for (k in 1:K){
@@ -28,4 +29,29 @@ extract_latent_class <- function(fit) {
   }
 
   p_z
+}
+
+#' Extract prevalance information
+#'
+#' Extract prevalence/pi estiamtes from a fit object
+#'
+#' @param fit fit object
+#' @return data.frame of estimated mean category probaility and standard deviation
+#'
+#' @export
+extract_prevalance <- function(fit) {
+
+  validate_fit(fit)
+
+  pi_samps <- rstan::extract(fit$draws)$pi
+
+  pi_mean <- apply(pi_samps, 2, mean)
+  pi_sd   <- apply(pi_samps, 2, sd)
+
+  out <- data.frame(category = 1:ncol(pi_samps),
+                    prob = pi_mean,
+                    sd = pi_sd)
+
+  out
+
 }
