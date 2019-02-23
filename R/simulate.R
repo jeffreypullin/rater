437e77af64cc1d655268919c792216d1b43babc8
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-02-23 18:29:01 +1100

    overhaul simulate interface

--- R/simulate.R
+++ R/simulate.R
@@ -14,21 +14,16 @@
 #'
 #' @examples
 #'
-# Question: is data_list the best way to refer to the object in the UI?
-simulate.fit <- function(object,
-                         nsim = 1,
-                         seed = NULL,
-                         new_params = NULL,
-                         new_data_list = NULL,
-                         ...) {
+simulate_model <- function(model, parameters, data_list, ...) {
 
-  model_type <- class(object)[[1]]
+  model_type <- class(model)[[1]]
   out <- switch(model_type,
-      "dawid_skene" = simulate_ds(object, ...),
-      "hier_dawid_skene" = simulate_hierds(object, ...),
-      "multinomial" = simulation_multi(object, ...))
+      "dawid_skene" = simulate_ds(model, parameters, data_list, ...),
+      "hier_dawid_skene" = simulate_hierds(model, parameters, data_list, ...),
+      "multinomial" = simulation_multi(model, parameters, data_list, ...))
 
   out
+
 }
 
 #' Internal funtion to simulate the dawid and skene model
@@ -44,31 +39,19 @@ simulate.fit <- function(object,
 #'   * The slightly clunky interface for new model simulation is due to needing
 #'     to use the stats::simulate generic
 #'
-simulate_ds <- function(object, ...) {
+simulate_ds <- function(model, parameters, data_list, ...) {
 
   args <- list(...)
   if (!is.null(args$seed)) {
-    set.seed(args$eed)
-  }
-
-  if (!is.null(args$new_data_list)) {
-    data_list <- args$new_data_list
-  } else {
-    data_list <- object$new_data_list
+    set.seed(args$seed)
   }
 
   I <- data_list$I # number of samples/patients (# of measurements)
   J <- data_list$J # tests/raters (# making observations)
   K <- data_list$K # categories/condition statuses
 
-  if (!is.null(args$new_params)) {
-    pi <- args$new_params$pi # prevalance
-    theta <- args$new_params$theta # rater probablistic confusion matrix
-  } else {
-    # get the fitted parameters from the model object
-    pi <- extract_prevalance(fit)$pi_mean
-    theta <- as_array(extract_raters(fit))
-  }
+  pi <- parameters$pi # prevalance
+  theta <- parameters$theta # rater probablistic confusion matrix
 
   z <- sample(1:K, I, replace = TRUE, prob = pi)
   data <- matrix(0, nrow = I, ncol = J)
