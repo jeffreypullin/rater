0451d3f637dd8d6c12460489c8a08882989d1f64
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-01 20:50:04 +1100

    change pars validation and reinstate K par

--- R/models.R
+++ R/models.R
@@ -19,7 +19,8 @@ dawid_skene <- function(alpha = NULL, beta = NULL) {
   validate_parameters(parameters)
   m <- list(parameters = parameters,
             name = "Bayesian Dawid and Skene Model",
-            file = "dawid_skene")
+            file = "dawid_skene",
+            K = compute_K(parameters))
   class(m) <- c("dawid_skene", "rater_model")
   m
 }
@@ -33,7 +34,8 @@ hier_dawid_skene <- function(alpha = NULL) {
   validate_parameters(parameters)
   m <- list(parameters = parameters,
             name = "Bayesian Hierarchical Dawid and Skene Model",
-            file = "hierarchical_dawid_skene")
+            file = "hierarchical_dawid_skene",
+            K = compute_K(parameters))
   class(m) <- c("hier_dawid_skene", "rater_model")
   m
 }
@@ -46,7 +48,8 @@ multinomial <- function(alpha = NULL, beta = NULL) {
   validate_parameters(parameters)
   m <- list(parameters = parameters,
             name = "Bayesian Multinomial (Annotator pooled) Model",
-            file = "multinomial")
+            file = "multinomial",
+            K = compute_K(parameters))
   class(m) <- c("multinomial", "rater_model")
   m
 }
@@ -58,24 +61,32 @@ multinomial <- function(alpha = NULL, beta = NULL) {
 #'
 #' @param parameters a (named) list of parmeters
 #'
-validate_parameters <- function(parameters) {
-  beta <- parameters$beta
-  alpha <- parameters$alpha
+validate_parameters <- function(pars) {
+  beta <- pars$beta
+  alpha <- pars$alpha
 
-  if (!is.null(alpha)) {
-    if (class(alpha) != "numeric") {
+  if (!is.null(alpha) & class(alpha) != "numeric") {
       stop("alpha must be a numeric vector", call. = FALSE)
-    }
   }
-
-  if (!is.null(beta)) {
-    # assuming it won't be character...
-    if (class(beta) != "matrix" || length(unique(beta)) != 1) {
+  if (!is.null(beta) & (class(beta) != "matrix" || length(unique(beta)) != 1)) {
       stop("Beta must be a square numeric matrix", call. = FALSE)
-    }
-    if (length(alpha) != unique(dim(beta))) {
-      stop("Alpha and beta must have the same dimensions", call. = FALSE)
-    }
   }
+  if (!is.null(unlist(pars)) & length(alpha) != unique(dim(beta))) {
+      stop("Alpha and beta must have the same dimensions", call. = FALSE)
+  }
+}
 
+# takes the list of parameters - alpha/beta
+compute_K <- function(pars) {
+  if (is.null(unlist(pars))) {
+    # no K values specified
+    NULL
+  } else {
+    ks <- c(length(pars$alpha), unique(dim(pars$beta)))
+    # Note:
+    # we assume here that the consistency of parameter
+    # dimensions has already been checked
+    # stopifnot(length(unique(ks)) != 1)
+    ks[ks > 0]
+  }
 }
