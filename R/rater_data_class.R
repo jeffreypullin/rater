374fc6c927fdf0dd5ce797cb5e1f6325c8511ab9
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-02 22:15:01 +1100

    add more display methods

--- R/rater_data_class.R
+++ R/rater_data_class.R
@@ -19,7 +19,25 @@ summary.rater_data <- function(object, ...) {
   cat("rater data")
 }
 
+`[.rater_data` <- function(x, i, j, drop = TRUE) {
+  x$data[i, j, drop = drop]
+}
+
+`[[.rater_data` <- function(x, i, j, drop = TRUE) {
+  stop("Please use `[` to subset rater_data", call. = FALSE)
+}
+
+dim.rater_data <- function(x) {
+  dim(get_data(x))
+}
+
+as.matrix.rater_data <- function(x, ...) {
+  get_data(x)
+}
+
 is.rater_data <- function(x) {
   inherits(x, "rater_data")
 }
+
+
 
