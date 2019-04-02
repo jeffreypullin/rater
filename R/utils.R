dfe6607789878be3c46501af56db68880750fe2c
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-02 22:34:36 +1100

    clean up utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,3 @@
-get_data <- function(d) {
-  d$data
-}
-
-get_stan_data <- function(d) {
-  d$stan_data
-}
-
 #' Numerically stable log_sum_exp function
 #' @param x vector of real numbers
 logsumexp <- function (x) {
@@ -19,14 +11,6 @@ softmax <- function (x) {
   exp(x - logsumexp(x))
 }
 
-#' Check that a passed object is actually of type fit
-#' @param fit fit object
-validate_fit <- function(fit) {
-  if (all(class(fit) != "fit")) {
-    stop("Cannot plot a non-fit object", call. = FALSE)
-  }
-}
-
 #' Check hat there has been no divergent transitions or poor convergence
 #' @param draws stanfit object
 check_convergence <- function(draws) {
@@ -36,10 +20,5 @@ check_convergence <- function(draws) {
     warning("Some R-hat statistics were above 1.1. The model fit may be invalid",
             call. = FALSE)
   }
-
 }
 
-is.stanfit <- function(x) {
-  # this is S4 syntax...
-  is(x, "stanfit")
-}
