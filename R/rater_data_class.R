0657d6bd230163aa13b232d1d4a7dfeacda80e7a
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-02 22:49:44 +1100

    add documentation

--- R/rater_data_class.R
+++ R/rater_data_class.R
@@ -1,36 +1,44 @@
-# is beautify/annotation a possibiliy?
+# An S3 class for the data types used in this package
 
+#' @s3method
 print.wide_data <- function(x, ...) {
   cat("`wide` rater data\n")
   print(get_data(x))
 }
 
+#' @s3method
 print.long_data <- function(x, ...) {
   cat("`long` rater data\n")
   print(get_data(x))
 }
 
+#' @s3method
 print.multinomial_data <- function(x, ...) {
   cat("`multinomial` rater data\n")
   print(get_data(x))
 }
 
+#' @s3method
 summary.rater_data <- function(object, ...) {
   cat("rater data")
 }
 
+#' @s3method
 `[.rater_data` <- function(x, i, j, drop = TRUE) {
   x$data[i, j, drop = drop]
 }
 
+#' @s3method
 `[[.rater_data` <- function(x, i, j, drop = TRUE) {
   stop("Please use `[` to subset rater_data", call. = FALSE)
 }
 
+#' @s3method
 dim.rater_data <- function(x) {
   dim(get_data(x))
 }
 
+#' @s3method
 as.matrix.rater_data <- function(x, ...) {
   get_data(x)
 }
@@ -39,5 +47,12 @@ is.rater_data <- function(x) {
   inherits(x, "rater_data")
 }
 
+get_data <- function(d) {
+  d$data
+}
+
+get_stan_data <- function(d) {
+  d$stan_data
+}
 
 
