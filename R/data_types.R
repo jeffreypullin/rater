23bbcf1b078bef79de71a0b0a7829f2b0682e461
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-03 19:18:47 +1100

    add data type constructors

--- R/data_types.R
+++ R/data_types.R
@@ -4,47 +4,32 @@
 
 # should/can we use the wrapper around new_ pattern?
 
+# User friendly functions
 
 wide_data <- function(data) {
   if (!is.numeric(data) || class(data) != "matrix") {
     stop("Data must be a numeric matrix")
   }
-  d <- list(data = data, stan_data = wide_to_stan(data))
-  class(d) <- c("wide_data", "rater_data")
-  d
+  new_wide_data(data)
 }
 
 long_data <- function(data) {
   if (!is.numeric(data) || !(length(dim(data)) == 2) || ncol(data) != 3) {
     stop("Data must be a numeric matrix with three columns")
   }
-  d <- list(data = data, stan_data = long_to_stan(data))
-  class(d) <- c("long_data", "rater_data")
-  d
+  new_long_data(data)
 }
 
 multinomial_data <- function(data) {
   if (!is.numeric(data) || length(dim(data)) != 2 || ncol(data) != 2) {
     stop("Data must be a numeric matrix with two columns")
   }
-  d <- list(data = data, stan_data = multinomial_to_stan(data))
-  class(d) <- c("multinomial_data", "rater_data")
-  d
+  new_multinomial_data(data)
 }
 
+# Constructors
 
-# TODO
-# table_data <- function(coding, N) {
-#   # can't remmber the approapriate function
-#   cat("in production")
-# }
-
-# could also have tabled multinomial data....
-# but the model for multinomial is simpler and should run faster + plus not
-# core functioanllity
-
-
-wide_to_stan <- function(data) {
+new_wide_data <- function(data) {
   I <- nrow(data)
   J <- ncol(data)
   K <- max(data)
@@ -53,33 +38,44 @@ wide_to_stan <- function(data) {
   jj <- rep(1:J, J)[mask]
   y <- as.vector(data)[mask]
   N <- length(y)
-  list(N = N, I = I, J = J, K = K, ii = ii, jj = jj, y = y)
+  stan_data <- list(N = N, I = I, J = J, K = K, ii = ii, jj = jj, y = y)
+  d <- list(data = data, stan_data = stan_data)
+  class(d) <- c("wide_data", "rater_data")
+  d
 }
 
-multinomial_to_stan <- function(data) {
-  list(N = nrow(data),
-       K = max(data[, 2]),
-       I = max(data[, 1]),
-       ii = data[, 1],
-       y = data[, 2])
+new_long_data <- function(data) {
+  stan_data <- list(N = nrow(data),
+                    I = max(data[, 1]),
+                    J = max(data[, 2]),
+                    K = max(data[, 3]),
+                    ii = data[, 1],
+                    jj = data[, 2],
+                    y = data[, 3])
+  d <- list(data = data, stan_data = stan_data)
+  class(d) <- c("long_data", "rater_data")
+  d
 }
 
-long_to_stan <- function(data) {
-  list(N = nrow(data),
-       I = max(data[, 1]),
-       J = max(data[, 2]),
-       K = max(data[, 3]),
-       ii = data[, 1],
-       jj = data[, 2],
-       y = data[, 3])
+new_multinomial_data <- function(data) {
+  stan_data <- list(N = nrow(data),
+                    K = max(data[, 2]),
+                    I = max(data[, 1]),
+                    ii = data[, 1],
+                    y = data[, 2])
+  d <- list(data = data, stan_data = stan_data)
+  class(d) <- c("multinomial_data", "rater_data")
+  d
 }
 
-# Make this generic
-# validate_stan_data
 
-# these validators arw aimed at the consistency of the stan_data
-# TODO (BIG)
-#validate_long_data <-
+
+
+# TODO
+# table_data <- function(coding, N) {
+#   # can't remmber the approapriate function
+#   cat("in production")
+# }
 
 
 
