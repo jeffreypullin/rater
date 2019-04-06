c38f15464f3580535b3ff56f67b507e21631ecdc
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-06 19:20:09 +1100

    add tests for optim

--- tests/testthat/test_fit_class.R
+++ tests/testthat/test_fit_class.R
@@ -31,38 +31,15 @@ test_that("is.mcmc_fit works", {
 
 test_that("plot.fit dispatches correctly", {
 
-  # raters_plot <- plot_raters(ds_fit)
-  # expect_equal(plot(ds_fit, type = "raters"), raters_plot)
-  # expect_equal(plot(ds_fit, type = "theta"), raters_plot)
-  #
-  # latent_class_plot <- plot_latent_class(ds_fit)
-  # expect_equal(plot(ds_fit, type = "latent_class"), latent_class_plot)
-  # expect_equal(plot(ds_fit, type = "z"), latent_class_plot)
-  #
-  # prevalance_plot <- plot_prevalance(ds_fit)
-  # expect_equal(plot(ds_fit, type = "prevalance"), prevalance_plot)
-  # expect_equal(plot(ds_fit, type = "pi"), prevalance_plot)
+  raters_plot <- plot_theta(ds_fit)
+  expect_equal(plot(ds_fit, type = "theta"), raters_plot)
+
+  latent_class_plot <- plot_z(ds_fit)
+  expect_equal(plot(ds_fit, type = "z"), latent_class_plot)
+
+  prevalance_plot <- plot_pi(ds_fit)
+  expect_equal(plot(ds_fit, type = "pi"), prevalance_plot)
 
 })
 
-test_that("plot.extract errors properly", {
-  # expect_error(extract(ds_fit), "The param to extract must be specified")
-})
-
-
-test_that("extract.fit dispatches correctly", {
-
-  # raters_param <- extract_raters(ds_fit)
-  # expect_equal(extract(ds_fit, param = "raters"), raters_param)
-  # expect_equal(extract(ds_fit, param = "theta"), raters_param)
-  #
-  # latent_class_param <- extract_latent_class(ds_fit)
-  # expect_equal(extract(ds_fit, param = "latent_class"), latent_class_param)
-  # expect_equal(extract(ds_fit, param = "z"), latent_class_param)
-  #
-  # prevalance_param <- extract_prevalance(ds_fit)
-  # expect_equal(extract(ds_fit, param = "prevalance"), prevalance_param)
-  # expect_equal(extract(ds_fit, param = "pi"), prevalance_param)
-
-})
 
