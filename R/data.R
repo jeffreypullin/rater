6d56748c0cd9cbc9d636180c325056e4ae7e7dd1
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2019-04-07 10:14:07 +1000

    add rough caries doc

--- R/data.R
+++ R/data.R
@@ -4,10 +4,24 @@
 #' 5 anesthetists on 45 patients with 4 categories in 'long' format. Each row
 #' represents one unique rating or annotation
 #'
-#' @format A data frame with 315 rows and 3 variables:
+#' @format A matrix with 315 rows and 3 columns:
 #' \describe{
 #'   \item{ii}{item index, which item is being rated}
 #'   \item{jj}{rater index, which rater is doing the rater}
 #'   \item{y}{what rating was given}
 #' }
 "anesthesia"
+
+#' Caries ratings of whether caries are 'sound' or not (c...)
+#'
+#' A dataset containing anesthetist ratings of patient readiness for surgery for
+#' 5 anesthetists on 45 patients with 4 categories in 'long' format. Each row
+#' represents one unique rating or annotation
+#'
+#' @format A matrix
+#' \describe{
+#'   \item{ii}{item index, which item is being rated}
+#'   \item{jj}{rater index, which rater is doing the rater}
+#'   \item{y}{what rating was given}
+#' }
+"caries"
