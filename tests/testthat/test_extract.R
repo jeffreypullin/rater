42857d3802a74a196b3ab9bfdcfd6f2135ab06f2
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2020-05-26 18:27:49 +1000

    Update tests

--- tests/testthat/test_extract.R
+++ tests/testthat/test_extract.R
@@ -1,18 +1,17 @@
 context("extract")
 
-test_that("posterior_draws works", {
+test_that("posterior_samples works", {
 
   # We get a list if length(pars) > 1
-  expect_type(posterior_draws(ds_fit, pars = c("pi", "theta")), "list")
-
-  # And an array if length(pars) == 1
-  expect_type(posterior_draws(ds_fit, pars = c("pi")), "double")
+  expect_type(posterior_samples(ds_fit, pars = c("pi", "theta")), "list")
+  expect_named(posterior_samples(ds_fit, pars = c("pi", "theta")),
+               c("pi", "theta"))
 
   # We error if the user requests draws from z
-  expect_error(posterior_draws(ds_fit, pars = c("z")))
+  expect_error(posterior_samples(ds_fit, pars = c("z")))
 
   # Or if they pass an invalid pararmaeter
-  expect_error(posterior_draws(ds_fit, pars = c("nonsense")))
+  expect_error(posterior_samples(ds_fit, pars = c("nonsense")))
 })
 
 test_that("point estiamte output is named", {
