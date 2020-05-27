4f28dd1e50cfd35ef180ae4c9d4ce8f486d2d754
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2020-05-27 13:02:18 +1000

    Add tests

--- tests/testthat/test_extract.R
+++ tests/testthat/test_extract.R
@@ -14,6 +14,19 @@ test_that("posterior_samples works", {
   expect_error(posterior_samples(ds_fit, pars = c("nonsense")))
 })
 
+test_that("posterior_interval works", {
+
+  all_intervals <- posterior_interval(ds_fit)
+  expect_type(all_intervals, "double")
+  expect_equal(dim(all_intervals), c(84, 2))
+
+  pi_intervals <- posterior_interval(ds_fit, pars = "pi")
+  expect_equal(dim(pi_intervals), c(4, 2))
+
+  expect_error(posterior_interval(ds_fit, pars = "z"))
+})
+
+
 test_that("point estiamte output is named", {
   all_pars <- point_estimate(ds_fit)
   expect_named(all_pars, c("pi", "theta", "z"))
