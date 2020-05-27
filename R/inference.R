4227081ba3f3b0c9619162de9817f216fe425473
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2020-05-27 19:29:31 +1000

    Initial implementation of new interface

--- R/inference.R
+++ R/inference.R
@@ -1,45 +1,97 @@
 #' Fit noisy catergrical rating models using Stan
 #'
-#' @param data Data for the model in long format as a three column dataframe of
-#'   ii, jj, yy
+#' @param data TODO
 #' @param model Model to fit to data of class model
 #' @param method method the method used to fit the model
+#' @param data_format TODO
 #' @param inits the initialization points of the fitting algorithm
 #' @param ... extra parameters to be passed to the Stan fitting interface
 #'
 #' @return An object of type fit containing the fitted parameters
 #'
+#' @details TODO
+#'
 #' @importFrom rstan sampling optimizing
+#'
+#' @examples
+#' "TODO"
 #'
 #' @export
 #'
-rater <- function(data, model, method = "mcmc", inits = NULL, ...) {
-  method <- match.arg(method, choices = c("mcmc", "optim"))
-  validate_input(data, model)
-  stan_data_list <- get_stan_data(data)
+rater <- function(data,
+                  model,
+                  method = "mcmc",
+                  data_format = "long",
+                  inits = NULL,
+                  ...) {
 
-  # check the priors and data are consistent
+  method <- match.arg(method, choices = c("mcmc", "optim"))
+  data_format <- match.arg(data_format, choices = c("long", "grouped"))
+
+  data <- validate_input(data, model, data_format)
+  stan_data_list <- as_stan_data(data, data_format)
+
+  # Check the priors and data are consistent.
   check_K(stan_data_list, model)
 
-  # create the full passed info for stan and the inits
+  # Create the full passed info for stan and the inits.
   stan_data <- c(stan_data_list, parse_priors(model, stan_data_list$K))
 
   if (is.null(inits)) {
     inits <- creat_inits(model, stan_data_list)
   }
 
-  # TODO: this could be made more complex if automatic switching is used
-  file <- get_stan_file(data, model)
+  # TODO This could be made more complex if automatic switching is used.
+  file <- get_stan_file(data_format, model)
 
   if (method == "mcmc") {
-    draws <- rstan::sampling(stanmodels[[file]], stan_data, init = inits, ...)
-    out <- new_mcmc_fit(model = model, draws = draws, data = data)
+    samples <- rstan::sampling(stanmodels[[file]], stan_data, init = inits, ...)
+    out <- new_mcmc_fit(model = model, draws = samples, data = data)
   } else if (method == "optim") {
     estimates <- rstan::optimizing(stanmodels[[file]], stan_data, init = inits, ...)
     out <- new_optim_fit(model = model, estimates = estimates, data = data)
   }
 
   out
+}
+
+#' Convert validated passed data into data for Stan.
+#'
+#' @param data Validated passed data
+#' @param data_format String specifying the format of the data
+#'
+#' @details The function accepts validated data. So we know that the data
+#'   will be a data.frame with the appropriate column names. See
+#'   [validate_data()] for details.
+#'
+#' @return A list of component data parts as requrired by the Stan models.
+#'
+#' @noRd
+as_stan_data <- function(data, data_format) {
+
+  if (data_format == "long") {
+     stan_data <- list(
+       N = nrow(data),
+       I = max(data$item),
+       J = max(data$rater),
+       K = max(data$rating),
+       ii = data$item,
+       jj = data$rating,
+       y = data$rating
+      )
+  } else if (data_format == "grouped") {
+    tally <- data[, ncol(data)]
+    key <- data[, 1:(ncol(data) - 1)]
+    stan_data <- list(
+      N = nrow(data),
+      K = max(key),
+      J = ncol(key),
+      key = key,
+      tally = tally
+    )
+  }
+
+  stan_data
 }
 
 #' Converts default prior parameter specification to full priors
@@ -49,10 +101,12 @@ rater <- function(data, model, method = "mcmc", inits = NULL, ...) {
 #'
 #' @return the fully reliased prior parameters
 #'
+#' @noRd
 parse_priors <- function(model, K) {
   switch(get_file(model),
     "dawid_skene" = ds_parse_priors(model, K),
-    "class_conditional_dawid_skene" = class_conditional_ds_parse_priors(model, K),
+    "class_conditional_dawid_skene" =
+      class_conditional_ds_parse_priors(model, K),
     "hierarchical_dawid_skene" = hier_ds_parse_priors(model, K),
     stop("Unsupported model type", call. = FALSE))
 }
@@ -102,6 +156,7 @@ class_conditional_ds_parse_priors <- function(model, K) {
 #'
 #' @return intits in the format required by stan
 #'
+#' @noRd
 creat_inits <- function(model, stan_data) {
   # better to have another short unique id...
   K <- stan_data$K
@@ -120,6 +175,7 @@ creat_inits <- function(model, stan_data) {
 #'
 #' @return inits in the format required by stan
 #'
+#' @noRd
 dawid_skene_inits <- function(K, J) {
   pi_init <- rep(1/K, K)
   theta_init <- array(0.2 / (K - 1), c(J, K, K))
@@ -136,6 +192,7 @@ dawid_skene_inits <- function(K, J) {
 #'
 #' @return inits in the format required by stan
 #'
+#' @noRd
 class_conditional_dawid_skene_inits <- function(K, J) {
   pi_init <- rep(1/K, K)
   theta_init <- matrix(0.8, nrow = J, ncol = K)
@@ -149,45 +206,96 @@ class_conditional_dawid_skene_inits <- function(K, J) {
 #'
 #' @return the fully reliased prior parameters
 #'
+#' @noRd
 check_K <- function(stan_data, model) {
   # NB: this does not/cannot tell the user which of the pars is inconsistent
   # but we can return a vector (with NULLs and parse cleverly)
   if (!is.null(model$K) && (stan_data$K != model$K)) {
     stop("The number of categories is inconsistent between data and the prior",
-         "parameters", call. = FALSE)
+         " parameters", call. = FALSE)
   }
 }
 
 #' Helper get the correct stan file to run for model/data combination
 #'
-#' @param data a rater_data object
+#' @param data_format A string containing the specification of the data format
 #' @param model a rater_model object
 #'
 #' @return the name (no .stan) of the stan file that should be run
 #'
-get_stan_file <- function(data, model) {
-  # we assume here that only legal inputs are considered
-  if (is.grouped_data(data)) {
+#' @noRd
+get_stan_file <- function(data_format, model) {
+
+  file <- get_file(model)
+  # If the data is grouped overide this. We are assuming we have a
+  # valid model/format pair.
+  if (data_format == "grouped") {
     file <- "grouped_data"
-  } else {
-    file <- get_file(model)
   }
   file
 }
 
 #' Helper to check if passed data and model are valid and consistent
 #'
-#' @param data rater_data
-#' @param model rater_model
+#' @param data The `data` argument passed to [rater()]
+#' @param model The `model` argument passed to [rater()]
+#' @param data_format The `data_format` argument passed to [rater()]
 #'
-validate_input <- function(data, model) {
-  if (!is.rater_data(data)) {
-    stop("data must be a rater data type", call. = FALSE)
-  }
+#' @noRd
+validate_input <- function(data, model, data_format) {
+
   if (!is.rater_model(model)) {
-    stop("model must be a rater model", call. = FALSE)
+    stop("`model` must be a rater model object.", call. = FALSE)
   }
-  if (is.grouped_data(data) & !is.dawid_skene(model)) {
-    stop("grouped data can only be uses with the Dawid and Skene model", call. = FALSE)
+
+  if (data_format == "grouped" & !is.dawid_skene(model)) {
+    stop("Grouped data can only be usesed with the Dawid and Skene model",
+         call. = FALSE)
   }
+
+  validate_data(data, data_format)
 }
+
+#' Validate the data passed into the rater function
+#'
+#' @param data The `data` argument passed to [rater()]
+#' @param data_format The `data_format` argument passed to [rater()]
+#'
+#' @return Validated data. This will always be a data.frame with the
+#'   appropriate column names for the column names.
+#'
+#' @noRd
+validate_data <- function(data, data_format) {
+
+  # TODO: The error message in this function should refer to a vignette
+  # or vignette section about data format.
+
+  # Note that this test for allow things like tibbles to be accepted. We
+  # next use as.data.frame to standardise the input.
+  if (!inherits(data, "data.frame") &&  !inherits(data, "matrix")) {
+    stop("`data` must be a data.frame or matrix.", call = FALSE)
+  }
+  data <- as.data.frame(data)
+
+  if (data_format == "long") {
+
+    if (!ncol(data) == 3L) {
+      stop("`data` must have exactly three columns.")
+    }
+
+    if (!(all(c("rater", "item", "rating") %in% colnames(data)))) {
+      stop("`data` must have three columns with names: `rater`, `item`, and",
+           " `rating`.", call. = FALSE)
+    }
+
+  } else if (data_format == "grouped") {
+
+    last_col_name <- colnames(data)[[ncol(data)]]
+    if (!last_col_name == "n") {
+      stop("The last column must be named `n`.", call = FALSE)
+    }
+  }
+
+  data
+}
+
