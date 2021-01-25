f2137a3fd71c7dda38387adc18530d9948a82a4c
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2021-01-25 18:58:01 +1100

    Add posterior_predict function
    An implementation of another generic from {rstantools}
    
    Note that the HDS model is currently not supported.

---
+++ tests/testthat/test-posterior_predict.R
@@ -0,0 +1,37 @@
+test_that("posterior_predict works across model types", {
+  new_data <- data.frame(item = rep(1:2, each = J), rater = rep(1:J, 2))
+
+  # Should error.
+  expect_error(posterior_predict(hds_fit, new_data))
+  expect_error(posterior_predict(hds_fit_optim, new_data))
+
+  # Should work.
+  expect_error(posterior_predict(ds_fit, new_data), NA)
+  expect_error(posterior_predict(ccds_fit, new_data), NA)
+
+  expect_error(posterior_predict(ds_fit_optim, new_data), NA)
+  expect_error(posterior_predict(ccds_fit_optim, new_data), NA)
+
+  expect_error(posterior_predict(ds_fit_grouped, new_data), NA)
+  expect_error(posterior_predict(ds_fit_grouped_optim, new_data), NA)
+})
+
+test_that("posterior_predict respects the seed", {
+
+  new_data <- data.frame(item = rep(1:2, each = J), rater = rep(1:J, 2))
+  expect_equal(posterior_predict(ds_fit, new_data, seed = 430),
+               posterior_predict(ds_fit, new_data, seed = 430))
+
+})
+
+test_that("posterior_predict validates new_data correctly", {
+
+  expect_error(posterior_predict(ds_fit,
+                                 new_data = data.frame(a = 1, b = 2, c = 3)))
+
+  expect_error(posterior_predict(ds_fit,
+                                 new_data = data.frame(raters = 1, items = 3)))
+
+  wrong_new_data <- data.frame(item = rep(1:2, each = 6), rater = rep(1:6, 2))
+  expect_error(posterior_predict(ds_fit, wrong_new_data))
+})
