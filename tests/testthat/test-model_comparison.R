94abbcb322745eeec3c888ae7c1c774a0777e5c3
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2021-07-12 18:18:01 +1000

    Add `waic()` function for model comparison

--- tests/testthat/test-model_comparison.R
+++ tests/testthat/test-model_comparison.R
@@ -23,3 +23,27 @@ test_that("loo output is the right format", {
   expect_s3_class(loo_res, "loo")
 })
 
+test_that("waic errors appropriately", {
+  expect_error(
+    waic(ds_fit_grouped),
+    "waic is not supported for models fit using grouped data."
+  )
+  expect_error(
+    waic(ds_fit_optim),
+    "waic cannot be calculated for models fit using optimisation."
+  )
+})
+
+test_that("waic works (smoke test)", {
+  # Sensitive to sampling.
+  skip_on_cran()
+
+  expect_warning(waic(ds_fit))
+  expect_warning(waic(ccds_fit))
+  expect_warning(waic(hds_fit))
+})
+
+test_that("waic output is the right format", {
+  waic_res <- suppressWarnings(waic(ds_fit))
+  expect_s3_class(waic_res, c("waic", "loo"))
+})
