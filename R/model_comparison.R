df07a7f8b4703579d5a9f40626c66b5b6ec91bc2
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2021-07-12 18:18:01 +1000

    Add `waic()` function for model comparison

--- R/model_comparison.R
+++ R/model_comparison.R
@@ -81,3 +81,66 @@ loo.rater_fit <- function(x,
 
   loo
 }
+
+#' Compute the WAIC - a measure of model fit - of a rater fit object.
+#'
+#' @param x A `rater_fit` object. All model types are currently supported
+#'   except the basic Dawid-Skene model fit with grouped data.
+#' @param ... Other arguments passed.
+#'
+#' @return A waic/loo object.
+#'
+#' @details This function provides provides an additional method for model
+#' comparison, on top of the `loo()` function. In general we recommend that
+#' `loo()` is preferred: see the documentation of the loo package for details.
+#' Also, note the comments regarding model selection the the details section
+#' of `loo()`.
+#'
+#' @examples
+#'
+#' \donttest{
+#' fit_ds <- rater(anesthesia, "dawid_skene", verbose = FALSE, chains = 1)
+#' fit_ccds <- rater(anesthesia, "class_conditional_dawid_skene",
+#'                   verbose = FALSE, chains = 1)
+#'
+#' waic(fit_ds)
+#' waic(fit_ccds)
+#' }
+#'
+#' @references
+#'
+#' Watanabe, S. (2010). Asymptotic equivalence of Bayes cross validation and
+#' widely application information criterion in singular learning theory.
+#' *Journal of Machine Learning Research* 11, 3571-3594.
+#'
+#' Vehtari, A., Gelman, A., and Gabry, J. (2017a). Practical Bayesian model
+#' evaluation using leave-one-out cross-validation and WAIC.
+#' *Statistics and Computing*. 27(5), 1413--1432. doi:10.1007/s11222-016-9696-4
+#' ([journal version](https://link.springer.com/article/10.1007/s11222-016-9696-4),
+#'  [preprint arXiv:1507.04544](https://arxiv.org/abs/1507.04544)).
+#'
+#' @importFrom loo extract_log_lik waic
+#'
+#' @aliases waic
+#' @method waic rater_fit
+#' @importFrom loo waic
+#' @export
+#' @export waic
+#'
+waic.rater_fit <- function(x, ...) {
+
+  if (x$data_format == "grouped") {
+    stop("waic is not supported for models fit using grouped data.",
+         call. = FALSE)
+  }
+
+  if (inherits(x, "optim_fit")) {
+    stop("waic cannot be calculated for models fit using optimisation.",
+         call. = FALSE)
+  }
+
+  log_lik <- loo::extract_log_lik(x$samples, merge_chains = FALSE)
+  waic <- loo::waic(log_lik)
+
+  waic
+}
